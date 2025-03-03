527a5612a27314adf491b3f36d3009696cd2cdff
Miguel Ángel Márquez Posso <135051998+MiguelMarquezPosso@users.noreply.github.com>
2025-03-03 12:10:01 -0500

    Se creo el archivo de la solución iterativa y el que contiene la lista de números.

---
+++ taller_3/taller_3/iterative_solution.py
@@ -0,0 +1,10 @@
+
+def calculate_avg_std_iter(numbers):
+    """
+    Calcula el promedio y la desviación estándar de una lista de números
+    El promedio se calcula como la suma de los números dividido por la cantidad de números
+    La desviación estándar se calcula como la raíz cuadrada de la suma de la diferencia de cada número
+        con el promedio al cuadrado dividido por la cantidad de números
+    """
+    # TODO: implementar la función, ** sirve en python para elevar a una potencia
+
